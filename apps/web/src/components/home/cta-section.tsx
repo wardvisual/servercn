beaa5e03ac76b724329a1ee11a8be4334169f341
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-23 12:02:05 +0545

    style: update the style of cta-scetion

--- apps/web/src/components/home/cta-section.tsx
+++ apps/web/src/components/home/cta-section.tsx
@@ -1,33 +1,82 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon, PlusIcon } from "lucide-react";
+import { ArrowRightIcon } from "lucide-react";
 
 export default function CallToAction() {
   return (
     <section
       id="cta"
-      className="bg-[radial-gradient(35%_80%_at_25%_0%,--theme(--color-foreground/.08),transparent)] dark:bg-[radial-gradient(35%_80%_at_25%_0%,--theme(--color-foreground/.08),transparent)] relative mx-auto mb-18 flex w-full max-w-3xl flex-col justify-between gap-y-4 border-y px-4 py-20">
-      <PlusIcon
-        className="absolute top-[-12.5px] left-[-11.5px] z-1 size-6"
-        strokeWidth={1}
-      />
-      <PlusIcon
-        className="absolute top-[-12.5px] right-[-11.5px] z-1 size-6"
-        strokeWidth={1}
-      />
-      <PlusIcon
-        className="absolute bottom-[-12.5px] left-[-11.5px] z-1 size-6"
-        strokeWidth={1}
-      />
-      <PlusIcon
-        className="absolute right-[-11.5px] bottom-[-12.5px] z-1 size-6"
-        strokeWidth={1}
-      />
-
-      <div className="pointer-events-none absolute -inset-y-6 left-0 w-px border-l" />
-      <div className="pointer-events-none absolute -inset-y-6 right-0 w-px border-r" />
-
-      <div className="absolute top-0 left-1/2 -z-10 h-full border-l border-dashed" />
+      className="bg-muted relative mx-auto mb-18 flex w-full max-w-3xl flex-col justify-between gap-y-4 rounded-xl  px-4 py-20">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-radius-top-left absolute -inset-1">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 19v-6a8 8 0 0 1 8 -8h6" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-radius-top-left absolute -inset-1 text-muted-secondary">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 19v-6a8 8 0 0 1 8 -8h6" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-radius-top-right absolute -top-1 -right-1 text-muted-secondary">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 5h6a8 8 0 0 1 8 8v6" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-radius-bottom-left absolute -bottom-1 -left-1 text-muted-secondary">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M19 19h-6a8 8 0 0 1 -8 -8v-6" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-radius-bottom-right absolute -bottom-1 -right-1 text-muted-secondary">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M19 5v6a8 8 0 0 1 -8 8h-6" />
+      </svg>
 
       <h2 className="text-center text-xl font-semibold text-balance md:text-3xl">
         Stop building from scratch. Start building with ServerCN.
