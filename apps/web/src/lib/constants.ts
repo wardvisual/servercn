af63bb413cbbc82792ffbaa64215efe058d60c2a
Subrata Mondal <subratamondal@tutanota.com>
2026-03-05 12:06:57 +0530

    feat(web): add Ai query contant for open in ai feature

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -12,3 +12,48 @@ export const X_URL = "https://x.com/AavashDhami2127";
 export const GITHUB_URL = `${BASE_GITHUB_URL}/servercn`;
 
 export const SERVERCN_URL = "https://servercn.vercel.app";
+
+export const AI_QUERIES = [
+    {
+        label: "ChatGPT",
+        name: "chatgpt",
+        url: (prompt: string) => `https://chatgpt.com/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: true,
+    },
+    {
+        label: "Claude",
+        name: "claude",
+        url: (prompt: string) => `https://claude.ai/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: true,
+    },
+    {
+        label: "Google AI Studio",
+        name: "google-ai-studio",
+        url: (prompt: string) => `https://aistudio.google.com/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: true,
+    },
+    {
+        label: "Perplexity",
+        name: "perplexity",
+        url: (prompt: string) => `https://perplexity.ai/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: true,
+    },
+    {
+        label: "Grok",
+        name: "grok",
+        url: (prompt: string) => `https://grok.com/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: true,
+    },
+    {
+        label: "DeepSeek",
+        name: "deepseek",
+        url: (prompt: string) => `https://deepseek.com/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: false,
+    },
+    {
+        label: "Gemini",
+        name: "gemini",
+        url: (prompt: string) => `https://gemini.google.com/?prompt=${encodeURIComponent(prompt)}`,
+        isAvailable: false,
+    },
+] as const;
