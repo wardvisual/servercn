cd9b208d222440a478b8eeb9f13b16e93a993ec1
Subrata Mondal <subratamondal@tutanota.com>
2026-03-05 16:34:38 +0530

    docs(web): add open in Ai component

---
+++ apps/web/src/components/docs/open-in-ai.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+import { Clock, ExternalLink } from "lucide-react";
+import { getIconForAi, type AiName } from "./icons/ai-icons";
+import { AI_QUERIES, SERVERCN_URL } from "@/lib/constants";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+
+import { usePathname } from "next/navigation";
+
+const defaultPrompt =
+  "Read this ServerCN documentation properly and provide information i ask. \nMake it clear and concise.";
+
+export function OpenInAi() {
+  const pathname = usePathname();
+  const [selectKey, setSelectKey] = useState(0);
+
+  const redirectToAi = (_ai: AiName) => {
+    const prompt = SERVERCN_URL + pathname + "\n" + defaultPrompt;
+
+    const aiurl = AI_QUERIES.find(q => q.name === _ai)?.url(prompt);
+
+    if (aiurl) {
+      window.open(aiurl, "_blank");
+    }
+
+    setSelectKey(prev => prev + 1);
+  };
+
+  return (
+    <div className="max-w-45">
+      <Select key={selectKey} onValueChange={redirectToAi}>
+        <SelectTrigger className="w-full md:w-44">
+          <SelectValue
+            placeholder={
+              <div className="flex items-center justify-center space-x-2">
+                <ExternalLink size={16} />
+                <p>Open in AI</p>
+              </div>
+            }
+          />
+        </SelectTrigger>
+        <SelectContent>
+          {AI_QUERIES.map(query => (
+            <SelectItem
+              key={query.name}
+              value={query.name}
+              disabled={!query.isAvailable}>
+              <div>{getIconForAi(query.name)}</div>
+              <p className="capitalize">{query.label}</p>
+              {!query.isAvailable && (
+                <span>
+                  <Clock size={16} />
+                </span>
+              )}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
