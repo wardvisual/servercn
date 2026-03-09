76b993f4b38ac4d21f4ef36f98e5c3e3daac91f1
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-09 08:54:54 +0545

    feat(models): add Mongoose post model and related types for blog application
    - Introduced Post model with schema defining title, slug, content, and other relevant fields
    - Implemented indexing for efficient searching and filtering of posts
    - Established constants for post statuses and TypeScript interfaces for enhanced type safety

---
+++ packages/templates/node/express/schema/blog-app/post/mongodb/mongoose/feature/src/modules/post/post.model.ts
@@ -0,0 +1,119 @@
+import mongoose, { Document, Model, Schema, Types } from "mongoose";
+import { PostStatus } from "./post.types";
+import { POST_STATUSES } from "./post.constants";
+
+interface IFile {
+  public_id: string;
+  url: string;
+  size: number;
+}
+
+export interface IPost extends Document {
+  _id: Types.ObjectId;
+
+  title: string;
+  slug: string;
+  content: string;
+
+  excerpt?: string;
+  author: Types.ObjectId;
+  category?: Types.ObjectId;
+  tags?: string[];
+  featuredImage: IFile | null;
+
+  views: number;
+  likes: Types.ObjectId[];
+  likeCount: number;
+
+  status: PostStatus;
+  publishedAt?: Date;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const postSchema = new Schema<IPost>(
+  {
+    title: {
+      type: String,
+      required: true
+    }, // Blog post title
+
+    slug: {
+      type: String,
+      required: true,
+    }, // SEO-friendly URL slug
+
+    content: {
+      type: String,
+      required: true
+    }, // Main blog content
+
+    excerpt: {
+      type: String
+    }, // Short summary
+
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }, // Reference to User
+
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category"
+    }, // Reference to category
+
+    tags: {
+      type: [String],
+      default: []
+    }, // List of tags
+
+    featuredImage: {
+      public_id: String,
+      url: String,
+      size: Number
+    }, // Blog cover image URL
+
+    views: {
+      type: Number,
+      default: 0
+    }, // View count
+
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ], // Users who liked the post
+
+    likeCount: {
+      type: Number,
+      default: 0
+    }, // Cached number of likes
+
+    status: {
+      type: String,
+      enum: POST_STATUSES,
+      default: "draft"
+    }, // Publication status
+
+    publishedAt: {
+      type: Date
+    } // Date when published
+  },
+  {
+    timestamps: true
+  }
+);
+
+//? indexes for efficient searching and filtering
+postSchema.index({ title: "text", content: "text" });
+postSchema.index({ author: 1, slug: 1 }, { unique: true });
+
+postSchema.index({ category: 1 }); // Efficient retrieval of posts by category
+postSchema.index({ createdAt: -1 }); // Efficient retrieval of posts by date
+const Post: Model<IPost> =
+  mongoose.models.Post || mongoose.model<IPost>("Post", postSchema);
+
+export default Post;
